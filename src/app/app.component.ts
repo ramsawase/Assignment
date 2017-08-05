f8c9a8c6ed39588b5391ffa0908051375a526e64
Rameshwar <ramsawase77@gmail.com>
2017-08-05 11:03:15 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  private WIN_LOGO = "./assets/images/WIN-.png";
+  private USER = "./assets/images/user.png"
+  private LOCK = "./assets/images/lock.png"
+}
